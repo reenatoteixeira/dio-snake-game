8eb6b3a0c34d630d0ba6b9c84752ed7da91a76c6
reenatoteixeira <renato.gfteixeira@outlook.com>
2022-04-15 04:06:59 -0300

    Minor changes

--- js/game.js
+++ js/game.js
@@ -8,7 +8,7 @@ const gameBoard = document.getElementById('game-board')
 
 function main(currentTime) {
     if (gameOver) {
-        if (confirm('You lost. Press OK to restart.')) {
+        if (confirm('Você perdeu! Aperte "OK" para reiniciar.')) {
             window.location = '/'
         }
         return
